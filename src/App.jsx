f83e0da585feebc61508b42bbe5079119ab1651f
ulf-frostypaw <19324157@uagro.mx>
2024-07-18 21:36:04 -0600

    Actualizacion para iniciar sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import Home from './pages/Home'
-import Login from './pages/Forms/Login'
-import Articles from './pages/Articles'
-import Start from './pages/Start'
-import Product from './pages/Product'
-import SideBar from './components/SideBar/SideBar'
-import NotFound from './pages/Error/NotFound'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import Login from "./pages/Forms/Login";
+import Articles from "./pages/Articles";
+import Start from "./pages/Start";
+import Product from "./pages/Product";
+import SideBar from "./components/SideBar/SideBar";
+import NotFound from "./pages/Error/NotFound";
+import { AuthContext } from "./components/Auth/AuthContext";
 
-function App() {
+function App() 
+{
+  const [email, setEmail] = "Keso";
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/sidebar" element={<SideBar />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/start" element={<Start />} />
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <AuthContext.Provider value={[email, setEmail]}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sidebar" element={<SideBar />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/start" element={<Start />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "../components/Layout";
 import Card from "../components/Product/Card";
 import Button from "../components/Button";
 import Heading from "../components/SectionHeader/Heading";
 import BestDeals from "../components/SectionHeader/BestDeals";
+import { AuthContext } from "../components/Auth/AuthContext";
 function Home() {
   const [productos, setProductos] = React.useState([]);
   const [error, setError] = React.useState(null);
+  const {email, setEmail} = useContext(AuthContext);
+  console.log("Email: " + email);
 
   React.useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -22,18 +25,24 @@ function Home() {
   return (
     <Layout title={"Inicio"}>
       <div className="container mx-auto my-12">
-      <BestDeals />
-        <Heading isCenter isMain desc="Seleccionamos estos productos para esta temporada de calor veraniego.">
-          Recomendados
-        </Heading>
-        <div className="grid gap-7 md:grid-cols-2 lg:grid-cols-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
+        <BestDeals />
+        <section>
+          <Heading
+            isCenter
+            isMain
+            desc="Seleccionamos estos productos para esta temporada de calor veraniego."
+          >
+            Recomendados
+          </Heading>
+          <div className="grid gap-7 md:grid-cols-2 lg:grid-cols-4">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </section>
       </div>
     </Layout>
   );
