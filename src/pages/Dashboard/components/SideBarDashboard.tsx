e2015ff5e88b14421f296745a58e759a5690ee7a
Luisander2651 <luisander2651@gmail.com>
2024-10-22 22:36:59 -0600

    Se añadio para facilidad de mediaqueris a la configuracion de taildwind, pd: no me deja iniciar sesion x´D

--- src/pages/Dashboard/components/SideBarDashboard.tsx
+++ src/pages/Dashboard/components/SideBarDashboard.tsx
@@ -13,7 +13,6 @@ import {
   faBars,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faVuejs } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
@@ -28,17 +27,15 @@ const Sidebar = () => {
   return (
     <div className="h-screen">
       <div
-        className={`w-64 bg-[#061c33] fixed h-[100%] flex flex-col ${
+        className={`w-64 bg-[#061c33] fixed h-full flex flex-col ${
           isOpen ? "translate-x-0 transition-transform duration-300" : "-translate-x-[80%] transition-transform duration-300"
         }`}
       >
         <div className="flex items-center justify-start text-white text-2xl font-bold py-4">
-          <div className="text-center text-white text-2xl font-bold py-4 ml-12 mr-4">
+          <img src={import.meta.env.VITE_APP_URL + "/ClimaPolar.png"} alt="" className="invert w-12 ml-2" />
+          <div className="text-center text-white text-2xl font-bold py-2 mr-1 ml-1">
             Clima polar
           </div>
-          <button onClick={toggleSidebar} className="ml-3 cursor-pointer">
-            <FontAwesomeIcon className={isOpen ? "rotate-0 transition-transform duration-300" : "rotate-180 transition-transform duration-300"} icon={faChevronLeft} />
-          </button>
         </div>
         <hr className="h-px border-[#ffffff33] border-1" />
         <nav className="flex-1">
@@ -62,35 +59,18 @@ const Sidebar = () => {
             </li>
           </ul>
         </nav>
-        {/* <div className="p-4 rounded-b-lg hover:bg-blue-700 bg-blue-600 flex items-center justify-center cursor-pointer">
-           <FontAwesomeIcon icon={faSignOutAlt} className="mr-3" />
-           Logout
-         </div> */}
+        {/* Botón en la esquina inferior derecha */}
+        <div className="absolute bottom-4 right-4 mr-1">
+          <button onClick={toggleSidebar} className="cursor-pointer text-white">
+            <FontAwesomeIcon 
+              className={` text-2xl ${isOpen ? "rotate-0 transition-transform duration-300" : "rotate-180 transition-transform duration-300"}`}
+              icon={faChevronLeft}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
 };
-// const [isOpen, setIsOpen] = useState(true);
-
-//   const toggleSidebar = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <div className="flex">
-//       <div className={`fixed top-0 left-0 h-full w-64 bg-indigo-500 transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-[80%]'}`}>
-//         <h2 className="text-white">Sidebar</h2>
-//         <button onClick={toggleSidebar} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded">
-//           Toggle Sidebar
-//         </button>
-//       </div>
-//       <div className="flex-grow p-4">
-//         <main>
-//           <h1>Main Content</h1>
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#00A0E4',
-        secondary: '#000000',
+        primary: "#00A0E4",
+        secondary: "#000000",
         daikinBlack: "#231F20",
-        gray: '#eeeeee',
+        gray: "#eeeeee",
+      },    
+      screens: {
+        phone: "576px",
+        // => @media (min-width: 640px) { ... }
+
+        tablet: "768px",
+        // => @media (min-width: 1024px) { ... }
       },
     },
   },
   plugins: [],
-}
+};
